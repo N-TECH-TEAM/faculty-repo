b980b4832da2eb4724a5aee21b38a0cb57d87b31
Aliyu Abdulrazak <106888664+Razak002@users.noreply.github.com>
2024-02-22 23:05:47 +0100

    update :)

--- src/nuesa/Page.js
+++ src/nuesa/Page.js
@@ -26,7 +26,7 @@ function Page() {
             <Navbar />
     
             <Routes>
-                <Route path='/' element={<Home />} />
+                <Route path='/'/>
                 <Route path='/about' element={<About />} />
                 <Route path='/gallery' element={<Gallery/>} />
                 <Route path='/contact' element={<Contact />} />
